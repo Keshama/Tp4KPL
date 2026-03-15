408bb4c793d0a8c4d18134225c8195c8f4bf9975
Keshama <keisyaspw@gmail.com>
2026-03-15 13:06:17 +0700

    second commit

---
+++ TP_MODUL4_103022400060/KodePos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP_MODUL4_103022400060
+{
+    public class KodePos
+    {
+        private Dictionary<string, string> tabelKodePos = new Dictionary<string, string>()
+        {
+            {"Batununggal", "40266"},
+            {"Kujangsari", "40287"},
+            {"Mengger", "40267"},
+            {"Wates", "40256"},
+            {"Cijaura", "40287"},
+            {"Jatisari", "40286"},
+            {"Margasari", "40286"},
+            {"Sekejati", "40286"},
+            {"Kebonwaru", "40272"},
+            {"Maleer", "40274"}
+        };
+        public string getKodePos(string kelurahan)
+        {
+            if (tabelKodePos.ContainsKey(kelurahan))
+            {
+                return tabelKodePos[kelurahan];
+            }
+            else
+            {
+                return "Kelurahan tidak ditemukan!";
+            }
+        }
+    }
+}
